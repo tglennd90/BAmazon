2162e92a860d7b29917f4c4c867139d73f2f31c9
Tatum Daily <tgd9019@outlook.com>
2020-03-11 15:32:20 -0500

    Initial Files (w/ template)

---
+++ bamazonCustomer.js
@@ -0,0 +1,70 @@
+// Initializes the npm packages used
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+require("console.table");
+
+// Initializes the connection variable to sync with a MySQL database
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "bamazon"
+});
+
+// Creates the connection with the server and loads the product data upon a successful connection
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+  }
+  loadProducts();
+});
+
+// Function to load the products table from the database and print results to the console
+function loadProducts() {
+  // Selects all of the data from the MySQL products table
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+
+    // Draw the table in the terminal using the response
+    console.table(res);
+
+    // Then prompt the customer for their choice of product, pass all the products to promptCustomerForItem
+    promptCustomerForItem(res);
+  });
+}
+
+// Prompt the customer for a product ID
+function promptCustomerForItem(inventory) {
+  // Prompts user for what they would like to purchase
+ 
+}
+
+// Prompt the customer for a product quantity
+function promptCustomerForQuantity(product) {
+}
+
+// Purchase the desired quantity of the desired item
+function makePurchase(product, quantity) {
+  
+}
+
+// Check to see if the product the user chose exists in the inventory
+function checkInventory(choiceId, inventory) {
+ 
+}
+
+// Check to see if the user wants to quit the program
+function checkIfShouldExit(choice) {
+  if (choice.toLowerCase() === "q") {
+    // Log a message and exit the current node process
+    console.log("Goodbye!");
+    process.exit(0);
+  }
+}
